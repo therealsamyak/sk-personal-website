b95022acdc7caf281307e436ac28efa3bc905642
therealsamyak <therealsamyak@gmail.com>
2026-05-29 20:54:07 -0700

    animation removed

--- apps/web/components/ProjectCard/ProjectCard.tsx
+++ apps/web/components/ProjectCard/ProjectCard.tsx
@@ -48,7 +48,7 @@ export const ProjectCard = ({
           <p className="fixed top-4 left-1/2 z-50 -translate-x-1/2 rounded-md bg-black/60 px-3 py-1 font-bold text-base text-white backdrop-blur-sm">
             Click outside to close
           </p>
-          <DialogPrimitive.Content className="data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] fixed top-[50%] left-[50%] z-50 w-full max-w-4xl translate-x-[-50%] translate-y-[-50%] duration-200 data-[state=closed]:animate-out data-[state=open]:animate-in">
+          <DialogPrimitive.Content className="data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 fixed top-[50%] left-[50%] z-50 w-full max-w-4xl translate-x-[-50%] translate-y-[-50%] duration-200 data-[state=closed]:animate-out data-[state=open]:animate-in">
             <DialogTitle>{title}</DialogTitle>
             <DialogPrimitive.Description className="sr-only">
               Full-size preview image for {title}. Press Escape or click outside to close.
